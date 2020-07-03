5dacae3d23ed117742543d122694f2ba7b213a43
ErdemT09 <erdemturkoglu40@gmail.com>
2020-07-03 10:31:30 +0300

    Day_2.1
    Player movement animations.1

--- Assets/Scripts/Player/p_movement.cs
+++ Assets/Scripts/Player/p_movement.cs
@@ -7,10 +7,12 @@ public class p_movement : MonoBehaviour
 	public float velocity;
 	private Rigidbody2D p_rb;
 	private Vector3 change;
+	private Animator animr;
 	//p_rb : Rigidbody2D of the player
     // Start is called before the first frame update
     void Start()
     {
+		animr = GetComponent<Animator>();
         p_rb = GetComponent<Rigidbody2D>();
     }
 
@@ -20,12 +22,24 @@ public class p_movement : MonoBehaviour
      change = Vector3.zero;   
 	 change.x = Input.GetAxisRaw("Horizontal");
 	 change.y = Input.GetAxisRaw("Vertical");
+	 Update_p_move();
 	 Debug.Log(change);
-	 if(change != Vector3.zero)
+	 		 
+    }
+	
+	void Update_p_move()
+	{
+		if(change != Vector3.zero)
 	 {
 		p_move();
-	 }		
-    }
+		animr.SetFloat("moveX", change.x);
+		animr.SetFloat("moveY", change.y);
+		animr.SetBool("moving", true);	
+	 }else 
+	 {
+		animr.SetBool("moving", false);	
+	 }
+	}
 	
 	void p_move()
 	{
